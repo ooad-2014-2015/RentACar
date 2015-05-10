7901764df39eefb908dee917c4f935b2b3092038
Ajla Alić <aalic2@etf.unsa.ba>
2015-05-10 23:25:33 +0200

    View Model

---
+++ Projekat - kod/2. ViewModel/IznajmljivanjeViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.ComponentModel;
+
+namespace Rent_A_Car
+{
+    //Glavni VIewModel (parent) koji cuva ostale 
+    class IznajmljivanjeViewModel: INotifyPropertyChanged
+    {
+
+        //Varijable
+        public ICommand iznajmi { get; set; }
+        private Iznajmljivanje iznajmljivanje;
+        public Iznajmljivanje Iznajmljivanje
+        {
+            get { return iznajmljivanje; }
+            set { iznajmljivanje = value; }
+        }
+        
+
+        //View Modeli za ostale forme
+        private LogInSesijaViewModel dijeteLoginSesijaViewModel;
+        private PlacanjeViewModel dijetePlacanjeViewModel;
+        private PracenjeGPSViewModel dijetePracenjeGPSViewModel;
+        private RezervacijaViewModel dijeteRezervacijaViewModel;
+
+        //Metode
+        public IznajmljivanjeViewModel()
+        {
+            iznajmljivanje = new Iznajmljivanje();
+        }
+
+
+        
+
+    }
+}
